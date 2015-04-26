198656ac57e7fda55d482902800efd93bb85ea2b
miyakelly <liangzhao0801@gmail.com>
2015-04-26 19:20:53 -0400

    test

--- wordcloud/cloud.js
+++ wordcloud/cloud.js
@@ -37,19 +37,45 @@ var background = svg.append("g"),
 
 d3.select(window).on("hashchange", hashchange);
 
-var form = d3.select("#form")
-    .on("submit", function() {
-      load(d3.select("#text").property("value"));
-      d3.event.preventDefault();
-    });
-form.selectAll("input[type=number]")
-    .on("click.refresh", function() {
-      if (this.value === this.defaultValue) return;
-      generate();
-      this.defaultValue = this.value;
-    });
-form.selectAll("input[type=radio], #font")
-    .on("change", generate);
+//already hide 
+// var form = d3.select("#form")
+//     .on("submit", function() {
+//       load(d3.select("#text").property("value"));
+//       d3.event.preventDefault();
+//     });
+// form.selectAll("input[type=number]")
+//     .on("click.refresh", function() {
+//       if (this.value === this.defaultValue) return;
+//       generate();
+//       this.defaultValue = this.value;
+//     });
+// form.selectAll("input[type=radio], #font")
+//     .on("change", generate);
+
+d3.select(".btn1").on("click", function() {
+  load(d3.select(".text1").property("value"));
+  d3.event.preventDefault();
+});
+
+d3.select(".btn2").on("click", function() {
+  load(d3.select(".text2").property("value"));
+  d3.event.preventDefault();
+});
+
+d3.select(".btn3").on("click", function() {
+  load(d3.select(".text3").property("value"));
+  d3.event.preventDefault();
+});
+
+d3.select(".btn4").on("click", function() {
+  load(d3.select(".text4").property("value"));
+  d3.event.preventDefault();
+});
+
+d3.select(".btn5").on("click", function() {
+  load(d3.select(".text5").property("value"));
+  d3.event.preventDefault();
+});
 
 // From Jonathan Feinberg's cue.language, see lib/cue.language/license.txt.
 var stopWords = /^(i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$/,
